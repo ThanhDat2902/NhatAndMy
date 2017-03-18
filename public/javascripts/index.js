593bbbc2f6182e8fc1e2662f8494c96854671c27
Mickey  Le <mickey@donald.speedport_w_724v_typ_a_05011603_05_020>
2017-03-18 17:32:27 +0100

    delte user

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -30,9 +30,7 @@ function initializeClock(id, endtime){
     daysSpan.innerHTML = ('0' + t.days).slice(-2);
     hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
     minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    if (screen.width>=641) {
-      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-    }
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
     if(t.total<=0){
       clearInterval(timeinterval);
